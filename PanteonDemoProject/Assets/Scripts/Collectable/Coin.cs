5e9da5708ac1dab6e537d42786fcb7d832bafabd
Simla Beken <38881186+Simla4@users.noreply.github.com>
2023-05-15 23:33:55 +0300

    style: obstacle animations

--- PanteonDemoProject/Assets/Scripts/Collectable/Coin.cs
+++ PanteonDemoProject/Assets/Scripts/Collectable/Coin.cs
@@ -1,12 +1,35 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using DG.Tweening;
 using UnityEngine;
 
 public class Coin : MonoBehaviour, ICollectable
 {
+    [SerializeField] private Transform targetPoint;
+
+    private float screenWidth, screenHeight;
+
+    private void Start()
+    {
+        screenWidth = Screen.width;
+        screenHeight = Screen.height;
+    }
+
     public void OnCollect()
     {
         EventManger.OnCollectCoin?.Invoke();
-        gameObject.SetActive(false);
+        MoveCoin();
+    }
+
+    private void MoveCoin()
+    {
+        Debug.Log("pos: " + targetPoint.position);
+        transform.DOLocalMove(targetPoint.position, 0.5f)
+            .OnComplete(() =>
+            {
+                gameObject.SetActive(false);
+            });
+
     }
 }
