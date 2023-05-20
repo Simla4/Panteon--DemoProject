182902f80cd7438cba1d499255cabcf4a853c3c5
Simla Beken <38881186+Simla4@users.noreply.github.com>
2023-05-20 03:28:03 +0300

    refactor: control of the rotating platform was done in a single script

--- PanteonDemoProject/Assets/Scripts/Obstacles/RotatingPlatform.cs
+++ PanteonDemoProject/Assets/Scripts/Obstacles/RotatingPlatform.cs
@@ -1,19 +1,17 @@
-using System;
-using System.Collections;
-using System.Collections.Generic;
-using DG.Tweening;
 using UnityEngine;
 
 public class RotatingPlatform : MonoBehaviour
 {
     [SerializeField] private float rotationSpeed = 2;
+    [SerializeField] private bool isPlatformRotatingRight;
     
     private void OnCollisionStay(Collision other)
     {
         if (other.gameObject.CompareTag("Player"))
         {
             var adjustSpeed = rotationSpeed * Time.deltaTime;
-            other.transform.position += Vector3.left * adjustSpeed;
+            
+            other.transform.position += (isPlatformRotatingRight ?Vector3.right : Vector3.left) * adjustSpeed;
         }
         
     }
