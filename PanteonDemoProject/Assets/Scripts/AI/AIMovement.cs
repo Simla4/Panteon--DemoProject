be22e4c2b7f64039aaa3d0603f922938008985ab
Simla Beken <38881186+Simla4@users.noreply.github.com>
2023-05-22 14:40:12 +0300

    the build of the final version has been taken

--- PanteonDemoProject/Assets/Scripts/AI/AIMovement.cs
+++ PanteonDemoProject/Assets/Scripts/AI/AIMovement.cs
@@ -1,8 +1,5 @@
 using System;
 using System.Collections;
-using System.Collections.Generic;
-using System.ComponentModel.Design;
-using UnityEditor.ShaderKeywordFilter;
 using UnityEngine;
 using UnityEngine.AI;
 
